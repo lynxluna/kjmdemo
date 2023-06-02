0f544dfdd3e91ea4c81ab4aa6668db73fa8eaee6
Didiet Noor <lynxluna@gmail.com>
2023-06-02 19:38:10 +0700

    Initial Commit
    This is a very advanced web server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Wilujeng Sumping"))
+	})
+	log.Println("Starting server at 3000...")
+	http.ListenAndServe(":3000", r)
+}
